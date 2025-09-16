c2ee5ab71aeee8460577c4bac57ede8009dbd754
Cteklitz <cteklitz@ufl.edu>
2025-09-16 19:37:22 -0400

    Add check item method
    checks the players inventory for an item

--- Player.py
+++ Player.py
@@ -3,9 +3,31 @@ import Items
 
 inventory = []
 
+# adds an item to iventory
 def addItem(item):
     if (len(inventory) < 7):
         inventory.append(item)
         return True
     else:
+        return False
+    
+# checks if an item is in the inventory (can check based on item object, id, or name); Probably try to use id mostly?
+def checkItem(_item):
+    if (type(_item) is Items.Item):
+        for item in inventory:
+            if _item == item:
+                return True
+        return False # return false if item not found
+    elif (type(_item) is int):
+        for item in inventory:
+                if _item.id == item.id:
+                    return True
+        return False # return false if item not found
+    elif (type(_item) is str):
+        for item in inventory:
+                if _item.name == item.name:
+                    return True
+        return False # return false if item not found
+    else:
+        print("ERROR: Item type not valid")
         return False
