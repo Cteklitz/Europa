c250d54c30cf2fd2dd88e98a6a01405fa4b6894d
Cteklitz <cteklitz@ufl.edu>
2025-09-17 13:26:05 -0400

    Add remove item method to Player
    allows items to be removed from the inventory. Does not do any affects the items should have, just for when those are handled elsewhere

--- Player.py
+++ Player.py
@@ -26,3 +26,25 @@ def checkItem(_item):
     else:
         print("ERROR: Item type not valid")
         return False
+
+# removes an item from the iventory, takes either id or item object
+# this is for removing items that dont have a global effect, like placing a quest item or dropping an item
+# use consumeItem for consumable items that should do something
+def removeItem(_item):
+    if (type(_item) is Items.Item):
+        for i in range(len(inventory)): # int itertor bc we need the index the item is found at to pop it
+            if _item.id == inventory[i].id:
+                inventory.pop(i)
+                return True
+        return False # return false if item not found
+    elif (type(_item) is str):
+        for i in range(len(inventory)):
+                if _item == inventory[i].id:
+                    inventory.pop(i)
+                    return True
+        return False # return false if item not found
+    else:
+        print("ERROR: Item type not valid")
+        return False
+        
+# TODO: Consume item
