e0d6fc77087408078117c58b1efa6bc2675dd05e
ginaabascal <ginabascal@gmail.com>
2025-10-16 15:56:41 -0400

    Unlit window asset added

--- Rooms/PinkLowerWing.py
+++ Rooms/PinkLowerWing.py
@@ -28,9 +28,11 @@ lights = [
 # Lighting
 light_pos = (50, 50)
 light_pos2 = (640, 50)
+light_pos3 = (300, 50)
 wall_lights = [
     LightSource(light_pos[0], light_pos[1], radius=60, strength = 220),
-    LightSource(light_pos2[0], light_pos2[1], radius=60, strength = 220)
+    LightSource(light_pos2[0], light_pos2[1], radius=60, strength = 220),
+    LightSource(light_pos3[0], light_pos3[1], radius = 10, strength=220)
 ]
 falloff = [LightFalloff(virtual_screen.get_size(), darkness = 140)]
 
@@ -43,6 +45,8 @@ Bookcase = pygame.image.load("Assets/Bookcase.png")
 Bookcase2 = pygame.image.load("Assets/Bookcase2.png")
 Lit = pygame.image.load("Assets/Lit.png")
 scaledLit = pygame.transform.scale(Lit, (384, 55))
+unlit = pygame.image.load("Assets/WindowUnlit.png")
+scaledUnlit = pygame.transform.scale(unlit, (396, 68))
 lockedDoor = pygame.image.load("Assets/LockedDoor1.png")
 lockedDoorRect = lockedDoor.get_rect(topleft=(31,95))
 lockedDoorRange = pygame.Rect(lockedDoorRect.x, lockedDoorRect.y-10, lockedDoorRect.width+10, lockedDoorRect.height+10)
@@ -179,34 +183,30 @@ def Room(screen, screen_res, events):
     if (lowerWingPower and power and level == 1) or Objects.getPinkPower():
         Assets.punch_light_hole(virtual_screen, dark_overlay, (virtual_screen.get_width()/2, virtual_screen.get_height()/2), 500, (0, 0, 0))
 
-    pygame.draw.rect(virtual_screen, (105,105,105), (80, 0, 384, 32))
-    pygame.draw.rect(virtual_screen, "black", (80, 0, 384, 32), 1)
+    # pygame.draw.rect(virtual_screen, (105,105,105), (80, 0, 384, 32))
+    # pygame.draw.rect(virtual_screen, "black", (80, 0, 384, 32), 1)
 
-    pygame.draw.rect(virtual_screen, (105,105,105), (80, 95, 384, 17))
-    pygame.draw.rect(virtual_screen, "black", (80, 95, 384, 17), 1)
+    # pygame.draw.rect(virtual_screen, (105,105,105), (80, 95, 384, 17))
+    # pygame.draw.rect(virtual_screen, "black", (80, 95, 384, 17), 1)
 
-    pygame.draw.rect(virtual_screen, "navyblue", (80, 31, 384, 64))
-    pygame.draw.rect(virtual_screen, "black", (80, 31, 384, 64), 1)
+    # pygame.draw.rect(virtual_screen, "navyblue", (80, 31, 384, 64))
+    # pygame.draw.rect(virtual_screen, "black", (80, 31, 384, 64), 1)
 
     if upperWingPower and power and level == 1 or Objects.getPinkPower():
         virtual_screen.blit(scaledLit, (79, 30))
     else:
-        pygame.draw.rect(virtual_screen, (105,105,185), (104, 39, 360, 40))
-        pygame.draw.rect(virtual_screen, "black", (104, 39, 360, 40), 1)
+        virtual_screen.blit(scaledUnlit, (83, 30))
 
-        pygame.draw.rect(virtual_screen, (0, 0, 30), (104, 50, 360, 20))
-        pygame.draw.rect(virtual_screen, "black", (104, 50, 360, 20), 1)
+    # pygame.draw.line(virtual_screen, (185, 122, 167), (133, 79), (133, 93), 5)
+    # pygame.draw.line(virtual_screen, (185, 122, 167), (163, 79), (163, 93), 5)
 
-    pygame.draw.line(virtual_screen, (185, 122, 167), (133, 79), (133, 93))
-    pygame.draw.line(virtual_screen, (185, 122, 167), (163, 79), (163, 93))
+    # pygame.draw.line(virtual_screen, (185, 122, 167), (316, 79), (316, 93), 5)
+    # pygame.draw.line(virtual_screen, (185, 122, 167), (346, 79), (346, 93), 5)
 
-    pygame.draw.line(virtual_screen, (185, 122, 167), (316, 79), (316, 93))
-    pygame.draw.line(virtual_screen, (185, 122, 167), (346, 79), (346, 93))
+    # pygame.draw.line(virtual_screen, "white", (100, 60), (125, 35))
+    # pygame.draw.line(virtual_screen, "white", (160, 75), (185, 50))
 
-    pygame.draw.line(virtual_screen, "white", (100, 60), (125, 35))
-    pygame.draw.line(virtual_screen, "white", (160, 75), (185, 50))
-
-    pygame.draw.rect(virtual_screen, (185, 122, 87), (630, 112, 20, 1))
+    # pygame.draw.rect(virtual_screen, (185, 122, 87), (630, 112, 20, 1))
 
     for x in range(160, 680, 80):
         virtual_screen.blit(Assets.squishedPipes[10], (x,112))
