ac345f4af0ea0fe57cf4e63f927ab854336aeda9
jayxd0909 <jayxd0909@gmail.com>
2025-11-13 21:17:57 -0500

    Added GreenPower check to lights in bathroom and greenhouse

--- Rooms/Bathroom.py
+++ Rooms/Bathroom.py
@@ -146,7 +146,7 @@ def Room(screen, screen_res, events):
             
     level, power = Objects.getPipeDungeonInfo()
     # Add greenpower statement
-    if level == 3 and power:
+    if (level == 3 and power) or Objects.getGreenPower():
         lit = True
     else:
         lit = False
